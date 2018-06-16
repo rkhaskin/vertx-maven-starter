4cc580c3477eb9562dee4f5d777f5bf440d12034
Rostislav Khaskin <rkhaskin@yahoo.com>
2018-06-16 17:17:50 -0400

    added main

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,6 +1,7 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -11,4 +12,8 @@ public class MainVerticle extends AbstractVerticle {
         .listen(8080);
   }
 
+  public static void main(String[] args) {
+	    Vertx vertx = Vertx.vertx();
+	    vertx.deployVerticle(new MainVerticle());
+	}
 }
